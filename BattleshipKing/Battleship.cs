98898cc2a2b9bd618da79a356588da917ac5b2fe
Jeff King <jazfunk@gmail.com>
2020-07-20 16:35:49 -0400

    First commit

---
+++ BattleshipKing/Battleship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleshipKing
+{
+    class Battleship
+    {
+        public Battleship()
+        {
+
+        }
+
+        public string BattleshipDirection { get; set; }
+        public int BattleshipLength { get; set; }
+        public Array FirstSegmentPosition { get; set; }
+        public int NumberOfHits { get; set; }
+
+    }
+}
